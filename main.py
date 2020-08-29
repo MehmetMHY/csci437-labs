c1e2e3a9093d287d4c811033fda41a374ee09caf
Mehmet <mehmet.mhy@gmail.com>
2020-08-29 14:25:43 -0600

    finished lab1. I will try to add comments and clean up the code

--- main.py
+++ main.py
@@ -1,33 +1,65 @@
 import cv2
 import sys
 import numpy as np
+import time
 
-def main():
+def recedeSqaure(video_file):
     # Read images from a video file in the current folder.
-    video_capture = cv2.VideoCapture("earth.wmv")     # Open video capture object
+    video_capture = cv2.VideoCapture(video_file)     # Open video capture object
     got_image, bgr_image = video_capture.read()       # Make sure we can read video
     if not got_image:
         print("Cannot read video source")
         sys.exit()
+    
+    img_height = bgr_image.shape[0]
+    img_width = bgr_image.shape[1]
+    cx = int(round(img_width/2))
+    cy = int(round(img_height/2))
+    fx = 500
+    fy = 500
+
+    # x, y, z
+    p1 = [-1.0, -1.0, 1.0]
+    p2 = [1.0, -1.0, 1.0]
+    p3 = [1.0, 1.0, 1.0]
+    p4 = [-1.0, 1.0, 1.0]
+
+    temp = 0
+    frame = 0
 
     # Read and show images until end of video is reached.
     while True:
         got_image, bgr_image = video_capture.read()
         if not got_image:
             break       # End of video; exit the while loop
-        # Draw a marker (a cross) in the middle of the image.
-        pos1 = cv2.drawMarker(bgr_image, position=(200,200), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
 
-        pos2 = cv2.drawMarker(bgr_image, position=(200,300), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
+        frame = frame + 1
 
-        pos3 = cv2.drawMarker(bgr_image, position=(400,200), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
+        x = int(fx * p1[0]) / (p1[2] + temp) + cx
+        y = int(fy * p1[1]) / (p1[2] + temp) + cy
+        pos1 = cv2.drawMarker(bgr_image, position=(int(x),int(y)), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
 
-        pos4 = cv2.drawMarker(bgr_image, position=(400,300), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
+        x = int(fx * p2[0]) / (p2[2] + temp) + cx
+        y = int(fy * p2[1]) / (p2[2] + temp) + cy
+        pos2 = cv2.drawMarker(bgr_image, position=(int(x),int(y)), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
+        
+        x = int(fx * p3[0]) / (p3[2] + temp) + cx
+        y = int(fy * p3[1]) / (p3[2] + temp) + cy
+        pos3 = cv2.drawMarker(bgr_image, position=(int(x),int(y)), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
 
-        cv2.imshow("my image", bgr_image)
+        x = int(fx * p4[0]) / (p4[2] + temp) + cx
+        y = int(fy * p4[1]) / (p4[2] + temp) + cy
+        pos4 = cv2.drawMarker(bgr_image, position=(int(x),int(y)), color=(0, 0, 255), markerType=cv2.MARKER_SQUARE)
+
+        cv2.putText(bgr_image, text=str(frame), org=(20, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0,255,0))
+        
+        cv2.imshow("Lab 1: OpenCV-Python Basics", bgr_image)
 
         # Wait for xx msec (0 means wait till a keypress).
         cv2.waitKey(30)
+        temp = temp + 0.1
 
 if __name__ == "__main__":
-    main()
+    recedeSqaure("earth.wmv")
+    print("Enter CTRL-C To Exit")
+    time.sleep(100)
