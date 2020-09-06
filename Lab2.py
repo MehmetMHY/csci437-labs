af8e5e4d2a849e6c4aa7ce0091f4bb38030cde84
Jean Duong <bduong@mymail.mines.edu>
2020-09-06 15:05:22 -0600

    maybe done with 2a

--- Lab2.py
+++ Lab2.py
@@ -9,3 +9,14 @@ def main():
     cx = math.cos(ax)
 
     Rx = np.array(((1, 0, 0), (0, cx, -sx), (0, sx, cx)))
+    R_c_w = Rx  # The only rotation is about x
+
+    H_c_w = np.block([[R_c_w, tc_w], [0, 0, 0, 1]])  # Get as 4x4 matrix
+    print("H_c_w:"), print(H_c_w)
+
+    # Get transformation from world to camera.
+    H_w_c = np.linalg.inv(H_c_w)
+    print("H_w_c:"), print(H_w_c)
+
+if __name__ == "__main__":
+    main()
